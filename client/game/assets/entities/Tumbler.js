60e31a709e613ebda553cedeb1300c1f857ab925
cavra <coryavra@gmail.com>
2016-11-24 17:55:58 -0600

    gravity

--- client/game/assets/entities/Tumbler.js
+++ client/game/assets/entities/Tumbler.js
@@ -15,7 +15,7 @@ Tumbler.prototype.create = function() {
     this.game.physics.enable(this.playerSprite, Phaser.Physics.ARCADE);
     this.playerSprite.anchor.set(0.5);
     this.playerSprite.body.allowGravity = true;
-    this.playerSprite.body.gravity.y = 2000;
+    this.playerSprite.body.gravity.y = 1000;
     this.playerSprite.body.maxVelocity.x = 375;
     this.playerSprite.body.maxVelocity.y = 1000;
     //this.playerSprite.body.collideWorldBounds = true;
